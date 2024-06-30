38114129f20b62c1e014ffd9d755f49621cd0b3d
Tarick Arnold <arnoldt2009@gmail.com>
2024-06-30 14:58:46 -0500

    Functioning bot

--- parse.py
+++ parse.py
@@ -4,28 +4,30 @@ import feedparser
 
 parser_logger = logging.getLogger(__name__) 
 
-def fetch_rss_data(url):
+def fetch_rss_data(urls):
     """Collect recent articles found on RSS or ATOM link"""
     # Get current year and current month
     current_year = datetime.now().year
     current_month = datetime.now().month
 
     # parse
-    feed = feedparser.parse(url)
-    parser_logger.info("RSS parsed")
-    for entry in feed.entries:
-        date = entry.published_parsed
-        if date.tm_year == current_year and date.tm_mon == current_month:
-            parser_logger.info("Articles sorted by month and year")
-            print("Entry Title:", entry.title)
-            print("Entry Author:", entry.author)
-            print("Entry Link:", entry.link)
-            print("Entry Published Date:", entry.published)
-            print("Entry Summary:", entry.summary)
-            print("\n")
-            # parser_logger.info(f"Article contents extracted. title:{entry.title},
-            # author:{entry.author}, link:{entry.link}, published_date:{entry.published}")                                )
-
+    for url in urls:
+        feed = feedparser.parse(url)
+        if len (feed) > 0:
+            parser_logger.info(f"RSS {url} parsed")
+            try:
+                for entry in feed.entries:
+                    date = entry.published_parsed
+                    if date.tm_year == current_year and date.tm_mon == current_month:
+                        print("Entry Title:", entry.title)
+                        print("Entry Author:", entry.author)
+                        print("Entry Link:", entry.link)
+                        print("Entry Published Date:", entry.published)
+                        print("Entry Summary:", entry.summary)
+                        print("\n")
+                        parser_logger.info(f"Article contents extracted. title:{entry.title} published date {entry.publshed}")
+            except: AttributeError
+            parser_logger.exception(f"Nothing found at {url}")
 def main():
     url = "https://www.theverge.com/rss/index.xml"
     fetch_rss_data(url)
--- search.py
+++ search.py
@@ -1,25 +1,42 @@
 import logging
 import os
-import feedsearch
+from bs4 import BeautifulSoup
+import requests
 from config import Config
 
 search_logger = logging.getLogger(__name__) 
 
-def fetch_rss_link(links) -> None:
-    """Locate the RSS or ATOM feed based on base url"""
-    urls: list[str] = []
+def fetch_rss_links(links)-> list:
+    urls: list = []
+    headers = {'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.3'}
     for link in links:
-        link = str(link)
-        feed: str = feedsearch.search(link)
-        urls.append(feed)
-        search_logger.info(f"Found {urls}")
+        response = requests.get(url= link, headers= headers).text
+        html = BeautifulSoup(response, 'lxml')
+        feed_urls = html.find_all("link", rel="alternate", 
+        type = ["application/atom+xml", "application/rss+xml", "application/rss.xml"])
+        
+        try:
+            for f in feed_urls:
+                if "rss" or "xml" or "feed" in f:
+                    href = f.get("href", None)
+                    if "https://" not in href:
+                        full_url = link+href
+                        urls.append(full_url)
+                        search_logger.info(f"Link combined link and href: {full_url}")
+                    else:
+                        urls.append(href)
+                        search_logger.info(f"Found url: {href}")
+        except: AttributeError
+        search_logger.exception(f"Href did not find a url")
+
+    return urls
 
 def main():
     dir_name: str = os.path.dirname(p=__file__)
     config_file = os.path.join(dir_name, "config.toml")
     config = Config(config_file)
     links: any = config['bot']['urls']['websites']
-    fetch_rss_link(links)
+    fetch_rss_links(links=links)
 
 if __name__=="__main__":
     main()
